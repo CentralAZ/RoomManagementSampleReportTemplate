2cebc19786c37813f3aacc1c041f4097942e480e
nairdo <nairdo@gmail.com>
2019-04-19 22:06:37 -0700

    + Example of a simple fake custom example Report Template for the Room Management plugin for Rock.

---
+++ Migrations/001_RegisterReportTemplate.cs
@@ -0,0 +1,46 @@
+﻿// <copyright>
+// Copyright by the Central Christian Church
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using Rock.Plugin;
+
+namespace org.rocksolidchurch.RoomManagementTemplates.Migrations
+{
+    [MigrationNumber( 1, "1.6.0" )]
+    public class RegisterReportTemplate : Migration
+    {
+        public override void Up()
+        {
+            // Register your custom ReportTemplate "entity type"... that last item on this next line is your own custom GUID.
+            RockMigrationHelper.UpdateEntityType( "org.rocksolidchurch.RoomManagementTemplates.SampleReportTemplate", "RockSolidChurch SampleExample Report Template", "org.rocksolidchurch.RoomManagementTemplates.SampleReportTemplate, org.rocksolidchurch.RoomManagementTemplates, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", false, true, "A76C387A-96EA-475C-81D9-ABD73C049E01" ); // use your own custom GUID
+
+            // Register the IsActive attribute for your custom report.  The last item on this next line is another new custom GUID for your attribute.
+            RockMigrationHelper.AddEntityAttribute( "org.rocksolidchurch.RoomManagementTemplates.SampleReportTemplate", "1EDAFDED-DFE6-4334-B019-6EECBA89E05A", "", "", "Active", "", "Should Service be used?", 0, "True", "62EC758F-6DD8-47EF-A5C7-F1786AAC6DE4" ); // yep, your own custom guid is the last item here.
+
+            // Now set the IsActive value to True on that new attribute you just added.
+            RockMigrationHelper.AddAttributeValue( "62EC758F-6DD8-47EF-A5C7-F1786AAC6DE4", 0, "True", Guid.NewGuid().ToString() );
+        }
+
+        public override void Down()
+        {
+            // Cleanup on uninstall by removing your custom IsActive attribute...
+            RockMigrationHelper.DeleteAttribute( "62EC758F-6DD8-47EF-A5C7-F1786AAC6DE4" );
+
+            // ...and your custom report template entity type.
+            RockMigrationHelper.DeleteEntityType( "A76C387A-96EA-475C-81D9-ABD73C049E01" );
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "org.rocksolidchurch.RoomManagementTemplates" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "org.rocksolidchurch.RoomManagementTemplates" )]
+[assembly: AssemblyCopyright( "Copyright ©  2019" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "6a4ed76b-40ec-4172-878c-39c68a7af7e4" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
